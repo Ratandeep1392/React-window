f18782c90f1c474bc78883df39815b356c7ac2f3
Ratandeep1392 <120476355+Ratandeep1392@users.noreply.github.com>
2022-12-13 19:26:34 +0530

    Initial commit

---
+++ src/components/List.tsx
@@ -0,0 +1,72 @@
+import React, { useRef, createContext } from "react";
+import AutoSizer from "react-virtualized-auto-sizer";
+import { VariableSizeList } from "react-window";
+import ListRow from "./ListRow";
+import faker from "faker";
+
+export const DynamicListContext = createContext<
+  Partial<{ setSize: (index: number, size: number) => void }>
+>({});
+
+const genItems = (size: number) => {
+  return new Array(size)
+    .fill("")
+    .map(() =>
+      faker.random.words(Math.floor(Math.random() * Math.floor(42)) + 1)
+    );
+};
+
+const List = () => {
+  const mockListItems = genItems(100);
+
+  const listRef = useRef<VariableSizeList | null>(null);
+
+  const sizeMap = React.useRef<{ [key: string]: number }>({});
+
+  const setSize = React.useCallback((index: number, size: number) => {
+    // Performance: Only update the sizeMap and reset cache if an actual value changed
+    if (sizeMap.current[index] !== size) {
+      sizeMap.current = { ...sizeMap.current, [index]: size };
+      if (listRef.current) {
+        // Clear cached data and rerender
+        listRef.current.resetAfterIndex(0);
+      }
+    }
+  }, []);
+
+  const getSize = React.useCallback((index) => {
+    return sizeMap.current[index] || 100;
+  }, []);
+
+  // Increases accuracy by calculating an average row height
+  // Fixes the scrollbar behaviour described here: https://github.com/bvaughn/react-window/issues/408
+  const calcEstimatedSize = React.useCallback(() => {
+    const keys = Object.keys(sizeMap.current);
+    const estimatedHeight = keys.reduce((p, i) => p + sizeMap.current[i], 0);
+    return estimatedHeight / keys.length;
+  }, []);
+
+  return (
+    <DynamicListContext.Provider value={{ setSize }}>
+      <AutoSizer>
+        {({ height, width }) => (
+          <VariableSizeList
+            ref={listRef}
+            width={width}
+            height={height}
+            itemData={mockListItems}
+            itemCount={mockListItems.length}
+            itemSize={getSize}
+            overscanCount={4}
+            // See notes at calcEstimatedSize
+            estimatedItemSize={calcEstimatedSize()}
+          >
+            {({ ...props }) => <ListRow {...props} width={width} />}
+          </VariableSizeList>
+        )}
+      </AutoSizer>
+    </DynamicListContext.Provider>
+  );
+};
+
+export default List;
